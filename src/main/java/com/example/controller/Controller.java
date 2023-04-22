272568cb4b2393c6785769f029a03a583f204246
0316lyh <2042971411@qq.com>
2023-04-22 21:44:36 +0800

    Add Controller

---
+++ src/main/java/com/example/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author :liangyuhang1
+ * @className :Controller
+ * @date :2023/4/22/21:42
+ */
+@RestController
+public class Controller {
+}

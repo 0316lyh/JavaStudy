722df44517b76bed48735ac46b79b8c329e235e7
0316lyh <2042971411@qq.com>
2023-04-22 21:41:10 +0800

    Add Config

---
+++ src/main/java/com/example/config/Config.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author :liangyuhang1
+ * @className :Config
+ * @date :2023/4/22/21:38
+ */
+@Configuration
+public class Config {
+}
